66e91c6e1f95b922c6993ce048622042d258f669
Shamblonaut <shaheembn@gmail.com>
2024-03-11 16:12:41 +0530

    Setup base using node and webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/style.css';
+
+const greeting = document.createElement('h1');
+greeting.textContent = 'Hello World! This is a great start, innit?';
+document.body.appendChild(greeting);
